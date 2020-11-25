ae34712f27a234b073ff649361c8e9d5fd3367ed
Matthew George Wilkie <matumilkie@live.co.uk>
2020-11-25 11:32:26 +0000

    Added layout (Generator)
    The Layout of the Generator Class is complete

--- RandomGenerator/src/main/ActorObjects/Generator.java
+++ RandomGenerator/src/main/ActorObjects/Generator.java
@@ -2,12 +2,29 @@ package main.ActorObjects;
 
 import javafx.util.Pair;
 import main.StructureObjects.AssetPack;
+import main.StructureObjects.Project;
 
 import java.util.ArrayList;
 
 public class Generator extends Actor{
     //Optional Fields
     private ArrayList<Pair<Actor,Integer>> stages;
+
+    //Constructor
+    public Generator(String name, Project project, AssetPack assetPack) {
+        super(name, project, assetPack);
+        this.stages = new ArrayList<>();
+    }
+
+    //Secondary Methods
+    ///Getters & Setters
+    public ArrayList<Pair<Actor, Integer>> getStages() {
+        return stages;
+    }
+    public void setStages(ArrayList<Pair<Actor, Integer>> stages) {
+        this.stages = stages;
+    }
+
 
     //Old Stuff
 //    public Generator(String name, AssetPack assetPack){}

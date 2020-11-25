298c583c6015cdcfe9f2dee39b312ab853cb4145
Matthew George Wilkie <matumilkie@live.co.uk>
2020-11-25 11:34:15 +0000

    Added layout (Selector)
    The Layout of the Selector Class is complete

--- RandomGenerator/src/main/ActorObjects/Selector.java
+++ RandomGenerator/src/main/ActorObjects/Selector.java
@@ -1,14 +1,33 @@
 package main.ActorObjects;
 
+import javafx.util.Pair;
 import main.FilterObjects.Option;
 import main.FilterObjects.Tag;
 import main.StructureObjects.AssetPack;
+import main.StructureObjects.Project;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Selector extends Actor{
     //Optional Fields
-    private HashMap<Tag, Integer> selectedTagsNWeights;
+    private HashMap<Tag, Integer> tagsNWeights;
+
+    //Constructor
+    public Selector(String name, Project project, AssetPack assetPack) {
+        super(name, project, assetPack);
+        this.tagsNWeights = new HashMap<>();
+    }
+
+    //Secondary Methods
+    ///Getters & Setters
+    public HashMap<Tag, Integer> getTagsNWeights() {
+        return tagsNWeights;
+    }
+    public void setTagsNWeights(HashMap<Tag, Integer> tagsNWeights) {
+        this.tagsNWeights = tagsNWeights;
+    }
+
 
 //    //Old Stuff
 //    public Selector(String name, AssetPack assetPack){}

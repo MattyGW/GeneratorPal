d0f290e9a2440a289f0c7065dc8789d0c321991b
Matthew George Wilkie <matumilkie@live.co.uk>
2020-11-25 11:52:57 +0000

    Added layout (Tag)
    The Layout of the Tag Class is complete

--- RandomGenerator/src/main/FilterObjects/Tag.java
+++ RandomGenerator/src/main/FilterObjects/Tag.java
@@ -5,10 +5,41 @@ import main.StructureObjects.AssetPack;
 import java.util.HashMap;
 
 public class Tag extends FilterObject {
+    //Required fields
+    private TagGroup tagGroup;
     //Optional fields
     private Integer weight;
-    private TagGroup tagGroup;
     private HashMap<Variable, Integer> variableInteractions;
+
+    //Constructor
+    public Tag(String name, String description, AssetPack assetPack, TagGroup tagGroup) {
+        super(name, description, assetPack);
+        this.weight = 0;
+        this.tagGroup = tagGroup;
+        this.variableInteractions = new HashMap<>();
+    }
+
+    //Secondary Methods
+    //Getters and Setters
+    public TagGroup getTagGroup() {
+        return tagGroup;
+    }
+    public void setTagGroup(TagGroup tagGroup) {
+        this.tagGroup = tagGroup;
+    }
+    public Integer getWeight() {
+        return weight;
+    }
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+    public HashMap<Variable, Integer> getVariableInteractions() {
+        return variableInteractions;
+    }
+    public void setVariableInteractions(HashMap<Variable, Integer> variableInteractions) {
+        this.variableInteractions = variableInteractions;
+    }
+
 
     //Old Stuff
 //    public Tag(String name, AssetPack assetPack){}

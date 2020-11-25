ebb25563a87a7e9e126d9fdfa12d8e2886880193
Matthew George Wilkie <matumilkie@live.co.uk>
2020-11-25 11:43:26 +0000

    Added layout (FilterObject)
    The Layout of the FilterObject Class is complete

--- RandomGenerator/src/main/FilterObjects/FilterObject.java
+++ RandomGenerator/src/main/FilterObjects/FilterObject.java
@@ -8,6 +8,35 @@ public abstract class FilterObject {
     private String description;
     private AssetPack assetPack;
 
+    //Constructor
+    public FilterObject(String name, String description, AssetPack assetPack) {
+        this.name = name;
+        this.description = description;
+        this.assetPack = assetPack;
+    }
+
+    //Secondary Methods
+    //Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public AssetPack getAssetPack() {
+        return assetPack;
+    }
+    public void setAssetPack(AssetPack assetPack) {
+        this.assetPack = assetPack;
+    }
+
+
     //old stuff
 //    //Constructors
 //    protected FilterObject() {
